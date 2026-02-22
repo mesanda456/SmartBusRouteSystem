889c538278c525bb2558272999921476eebb9e47
Mesanda Sethumika <sethumikamesanda@gmail.com>
2026-02-22 14:10:04 +0530

    all web compatability

---
+++ bus-router-planner/bus-router-planner/src/main/java/com/example/bus_router_planner/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.example.bus_router_planner.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:63342")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
